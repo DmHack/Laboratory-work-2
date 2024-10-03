4b4af0a8853c0b2b16f0b150d0a1b260837170f5
DmHack <Dmitrynarukov@yandex.ru>
2024-10-03 11:12:02 +0300

    First task completed

--- main.c
+++ main.c
@@ -6,19 +6,19 @@
 #include <stdlib.h>
 
 int zadanie1() {
-    int rezjum, count = 1;
+    int resjum, count = 1;
     srand(time(NULL));
     printf("Приветствуем вас в игре «Угадай число»\nВыберите режим(1-2):\n1) Программа загадывает число\n2) Вы загадываете число\n");
-    scanf("%d", &rezjum);
+    scanf("%d", &resjum);
 
-    if (rezjum == 1) {
+    if (resjum == 1) {
         int random = 1 + rand()%1000;
         int res = 0;
         printf("Программа выбрала случайное число, попытайтесь его удагать:\n");
         while (1) {
             scanf("%d", &res);
             if (res == random) {
-                printf("Вы угадали число! Поздравляем!\nВы угадали с %d попытки!", count);
+                printf("\nВы угадали число! Поздравляем!\nВы угадали с %d попытки!", count);
                 break;
             } else {
                 if (res > random) {
@@ -30,6 +30,33 @@ int zadanie1() {
                 }
             }
         }
+    } else if (resjum == 2) {
+        int random, randomBot, niz = 1, verh = 1000; count = 1;
+        char vubor[1];
+        printf("Введите число от 1 до 1000, которое программа будет угадывать:");
+        scanf("%d", &random);
+        randomBot = 1 + rand()%1000;
+        printf("Программа выбрала число = %d. Выберите > < =?\n", randomBot);
+        scanf("%s", vubor);
+        while (1) {
+            if (vubor[0] == '>') {
+                verh = randomBot;
+                count++;
+                randomBot = niz + rand()%(verh - niz + 1);
+                printf("Программа выбрала число = %d. Выберите > < =?\n", randomBot);
+                scanf("%s", vubor);
+            } else if (vubor[0] == '<') {
+                niz = randomBot;
+                count++;
+                randomBot = niz + rand()%(verh - niz + 1);
+                printf("Программа выбрала число = %d. Выберите > < =?\n", randomBot);
+                scanf("%s", vubor);
+            } else if (vubor[0] == '=') {
+                printf("\nПрограмма угадала число! Потрачено попыток = %d", count);
+                break;
+            }
+        }
+
     }
 }
 
